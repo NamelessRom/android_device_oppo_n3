e4a75ecca0e6fd6e8ae9667eb86cd602b9f9f77e
Alexander Martinz <eviscerationls@gmail.com>
2015-01-27 12:35:40 +0100

    include twrp configuration
    Change-Id: I454800e41b363c29c42de294c835e2972daee51a
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -26,3 +26,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# Recovery
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := twrp.fstab
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/twrp.fstab
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/etc
+include $(BUILD_PREBUILT)
