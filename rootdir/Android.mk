549ba24014859de2f170a1ba4dd08f2f96d5735d
Dan Pasanen <dan.pasanen@gmail.com>
2015-02-01 07:38:49 -0600

    n3: add twrp flags/config
    Change-Id: Idc37b08cb40e5be7ba43326bca49d669f43535c7

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -26,3 +26,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+ # TWRP
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := twrp.fstab
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.twrp
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
+include $(BUILD_PREBUILT)
